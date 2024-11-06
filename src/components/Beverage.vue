602d61517ae275ab40fa4d6923d399317122037e
Katie Stacey <katiestacey@DESKTOP-SS5A2AQ.gvsu.edu>
2024-11-06 13:27:00 -0500

    working

--- src/components/Beverage.vue
+++ src/components/Beverage.vue
@@ -7,21 +7,18 @@
         <template #top>
           <Creamer 
             v-if="showCreamer" 
-            :creamer="creamer" 
-            :style="{ zIndex: creamerZIndex }"
+            :creamer="creamer"
           />
         </template>
         <template #mid>
           <Syrup 
             v-if="showSyrup" 
-            :syrup="syrup" 
-            :style="{ zIndex: syrupZIndex }"
+            :syrup="syrup"
           />
         </template>
         <template #bottom>
           <Base 
-            :base="base" 
-            :style="{ zIndex: baseZIndex }"
+            :base="base"
           />
         </template>
       </Contents>
@@ -50,47 +47,4 @@ const props = defineProps<Props>();
 
 const showCreamer = computed(() => props.creamer !== 'No Cream');
 const showSyrup = computed(() => props.syrup !== 'No Syrup');
-
-// Adjust z-index values
-const baseZIndex = computed(() => 1);
-const syrupZIndex = computed(() => showSyrup.value ? 2 : 0);
-const creamerZIndex = computed(() => showCreamer.value ? 3 : 0);
 </script>
-
-<style scoped>
-.mug-container {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-
-.contents {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  overflow: hidden;
-}
-
-.base, .syrup, .creamer {
-  position: absolute;
-  left: 0;
-  right: 0;
-}
-
-.base {
-  bottom: 0;
-  height: 100%;
-}
-
-.syrup {
-  bottom: 0;
-  height: 25%; 
-}
-
-.creamer {
-  top: 0;
-  height: 30%; 
-}
-</style>
